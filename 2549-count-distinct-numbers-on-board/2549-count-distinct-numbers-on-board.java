54f22ae6788ec8fdbc48a1b1fbc6950a77094ff0
thulasidharan23 <142580913+thulasidharan23@users.noreply.github.com>
2024-05-05 06:10:38 +0530

    Time: 2 ms (11.17%), Space: 40.4 MB (32.23%) - LeetHub

--- 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.java
+++ 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.java
@@ -1,5 +1,23 @@
 class Solution {
+    int help(int n){
+        int count=0;
+        if(n==1){
+            return 1;
+        }
+        if(n==2){
+            return 1;
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=n;j++){
+                if(n%j==1){
+                    count=i;
+                }
+            }
+        }
+        return count;
+    }
     public int distinctIntegers(int n) {
-        return Math.max(1,n-1);
+        int ans=help(n);
+        return ans;
     }
 }
