541b6cc3f59f3fbe7dbce7b9f1680abc59c26577
thulasidharan23 <142580913+thulasidharan23@users.noreply.github.com>
2024-05-08 22:05:37 +0530

    Time: 4 ms (67.97%), Space: 40.6 MB (15.35%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -1,13 +1,13 @@
 class Solution {
   public boolean judgeSquareSum(int c) {
-    long l = 0;
-    long r = (int) Math.sqrt(c);
+    long l=0;
+    long r=(int)Math.sqrt(c);
 
-    while (l <= r) {
-      long sum = l * l + r * r;
-      if (sum == c)
+    while(l<=r){
+      long sum=l*l+r*r;
+      if(sum==c)
         return true;
-      if (sum < c)
+      if(sum<c)
         ++l;
       else
         --r;
