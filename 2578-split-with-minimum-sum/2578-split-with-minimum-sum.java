bee61c64c78c067b32793755bc3eb3f7b5bbcf1b
thulasidharan23 <142580913+thulasidharan23@users.noreply.github.com>
2024-06-28 11:59:11 +0530

    Time: 1 ms (87.57%), Space: 40.1 MB (91.62%) - LeetHub

---
+++ 2578-split-with-minimum-sum/2578-split-with-minimum-sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int splitNum(int num) {
+        List<Integer>lt=new ArrayList<>();
+        int nums=num;
+        while(nums>0){
+            int rem=nums%10;
+            lt.add(rem);
+            nums/=10;
+        }
+        int even=0;
+        int odd=0;
+        Collections.sort(lt);
+        for(int i=0;i<lt.size();i++){
+            if(i%2==0){
+                even=even*10+lt.get(i);
+            }
+            else{
+                odd=odd*10+lt.get(i);
+            }
+        }
+        int ans=odd+even;
+        return ans;
+    }
+}
