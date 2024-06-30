bb2c20aa18d14eaaffc0259ba12d87940d13aa26
thulasidharan23 <142580913+thulasidharan23@users.noreply.github.com>
2024-06-30 22:06:14 +0530

    Time: 0 ms (100%), Space: 40.5 MB (19.28%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        int n1=1,n2=1,n3=0;
+        if(n<=1){
+            return n;
+        }
+        for(int i=2;i<=n;i++){
+            n3=n1+n2;
+            n1=n2;
+            n2=n3;
+        }
+    return n3;
+    }
+}
