c1221c82c6c563a2b88699ce68dbcd42bc26d57c
thulasidharan23 <142580913+thulasidharan23@users.noreply.github.com>
2024-07-02 16:13:26 +0530

    Time: 4 ms (8.61%), Space: 42.1 MB (6.55%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.java
+++ 2315-count-asterisks/2315-count-asterisks.java
@@ -3,7 +3,6 @@ class Solution {
         String str[]=s.split("[|]");
         int sum=0;
         for(int i=0;i<str.length;i++){
-            System.out.print(str[i]);
             int count=0;
             if(i%2==0){
                 for(int j=0;j<str[i].length();j++){
