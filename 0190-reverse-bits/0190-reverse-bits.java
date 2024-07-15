7d5dd0f125e3b263cbc5ea83665c246f3cbe2086
thulasidharan23 <142580913+thulasidharan23@users.noreply.github.com>
2024-07-15 06:11:04 +0530

    Time: 0 ms (100%), Space: 42 MB (16.74%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,9 +1,6 @@
 public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
-        String s=String.valueOf(n);
-        int k=Integer.reverse(n);
-        return k;
-        
+        return Integer.reverse(n);
     }
 }
