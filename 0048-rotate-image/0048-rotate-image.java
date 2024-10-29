aefe324f055b3f8905f3aebe65fa16149acd868b
thulasidharan23 <142580913+thulasidharan23@users.noreply.github.com>
2024-10-29 19:17:02 +0530

    Time: 0 ms (100%), Space: 42.2 MB (27.22%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -15,5 +15,21 @@ class Solution {
                 matrix[i][n-j-1]=temp;
             }
         }
+
+        // for(int i=0;i<n;i++) { //Anti-clockwise rotation 
+        //     for(int j=0;j<n/2;j++){
+        //         int temp=matrix[j][i];
+        //         matrix[j][i]=matrix[n-j-1][i];
+        //         matrix[n-j-1][i]=temp;
+        //     }
+        // }
+
+        // for(int i=0;i<n;i++) { //180 Degree Rotation
+        //     for(int j=0;j<(n+1)/2;j++) {
+        //         int temp=matrix[i][j];
+        //         matrix[i][j]=matrix[n-i-1][n-j-1];
+        //         matrix[n-i-1][n-j-1]=temp;
+        //     }
+        // }
     }
 }
